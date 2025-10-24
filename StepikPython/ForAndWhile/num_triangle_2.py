802ef90f0baba1e8119dc67e92943bbad827f5a1
Leka_py <walt.teck@gmail.com>
2025-10-24 11:56:34 +0300

    This num triangle

--- StepikPython/ForAndWhile/num_triangle_2.py
+++ StepikPython/ForAndWhile/num_triangle_2.py
@@ -16,3 +16,13 @@
 Программа должна вывести треугольник в соответствии с условием.
 
 Примечание. Используйте вложенный цикл for.'''
+
+n = int(input())
+counter = 0
+
+for i in range(1, n + 1):
+    print()
+    for j in range(1, i + 1):
+        counter += 1
+        print(counter, end='')
+print()
